8a27c20e38ffd2ed295b4f3b30c8aa08b807a135
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2025-11-12 04:00:25 +0800

    doc: fix link

--- scripts/gen_data.py
+++ scripts/gen_data.py
@@ -30,9 +30,10 @@ TIPS_DATA_MD = """# BMS难度表镜像
 
 ## 用于BeMusicSeeker的难度表清单链接：
 
-- （Gitee直链）[gitee.com](https://gitee.com/MiyakoMeow/bms-table-mirror/raw/main/outputs/tables.json)
-- （GitHub中间链接）[get.2sb.org](https://get.2sb.org/https://github.com/MiyakoMeow/bms-table-mirror/raw/refs/heads/main/outputs/tables.json)
-- （GitHub中间链接）[gh-proxy.com](https://gh-proxy.com/https://github.com/MiyakoMeow/bms-table-mirror/raw/refs/heads/main/outputs/tables.json)
+- （Gitee直链）[gitee.com](https://gitee.com/MiyakoMeow/bms-table-mirror/raw/main/outputs/tables_gitee.json)
+- （中间链接）[get.2sb.org](https://get.2sb.org/https://github.com/MiyakoMeow/bms-table-mirror/raw/refs/heads/main/outputs/tables_intermediate_2sb.json)
+- （GitHub中间链接）[get.2sb.org](https://get.2sb.org/https://github.com/MiyakoMeow/bms-table-mirror/raw/refs/heads/main/outputs/tables_2sb.json)
+- （GitHub中间链接）[gh-proxy.com](https://gh-proxy.com/https://github.com/MiyakoMeow/bms-table-mirror/raw/refs/heads/main/outputs/tables_gh_proxy.json)
 - （GitHub直链）[raw.githubusercontent.com](https://github.com/MiyakoMeow/bms-table-mirror/raw/refs/heads/main/outputs/tables.json)
 
 ### 用法参考：
